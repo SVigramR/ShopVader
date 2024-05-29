de04ab296aca86701039306c407a7136a581fb4c
SVigramR <sabareeshvigram654@gmail.com>
2024-05-30 02:05:19 +0530

    Refactored quantity component and styled it

--- src/components/shop/Quantity.jsx
+++ src/components/shop/Quantity.jsx
@@ -1,15 +1,21 @@
+import { useState, useRef } from 'react';
 import quant from '/src/styles/quantity.module.css'
 
 function Quantity() {
+    const [quantity, setQuantity] = useState(0);
+   
+    const increment = () => setQuantity((countValue) => countValue + 1);
+    const decrement = () => setQuantity((countValue) => countValue - 1);
+
     return (
         <>
             <div className={quant.quant}>
-                <div>
-                    <button className={quant.quantControl}>-</button>
-                    <input type="text" defaultValue={0} />
-                    <button className={quant.quantControl}>+</button>
+                <div className={quant.count}>
+                    <button disabled={quantity <= 0} className={quant.quantControl} onClick={decrement}>-</button>
+                    <p className={quant.result}>{quantity}</p>
+                    <button  disabled={quantity >= 100} className={quant.quantControl} onClick={increment}>+</button>
                 </div>
-                <button className={quant.btn}>Cart</button>
+                <button className={quant.btn} >Cart</button>
             </div>
         </>
     )
